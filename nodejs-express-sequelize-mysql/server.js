2653d4f7946ae79f7cbb08730500ea87b1913b4b
giannigenovese1 <giannigenovese001@gmail.com>
2023-02-14 14:21:19 +0100

    restoring models, db and controller files with the bug

--- nodejs-express-sequelize-mysql/server.js
+++ nodejs-express-sequelize-mysql/server.js
@@ -25,3 +25,7 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+
+const db = require("./app/models");
+db.sequelize.sync();
